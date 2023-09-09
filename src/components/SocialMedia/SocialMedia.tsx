3643acc84d99ba294fcd90efb6ca016adbbea0fa
Thibaut <thibaut.moncourtois@gmail.com>
2023-09-09 10:50:29 +0200

    changement de logo et des liens (LinkedIn & GitHub)

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,20 +1,20 @@
-import TwitterLogo from '@assets/twitter-logo.png';
+import GithubLogo from '@assets/github-logo.png';
 import LinkedInLogo from '@assets/linkedin-logo.png';
 
 function socialMedia() {
   return (
     <section className="logos">
-      <div className="twitter-logo">
-        <a href="https://twitter.com/Thibz_GD">
-          <header className="absolute top-8 left-20">
-            <img className="w-12" src={TwitterLogo} alt="logoX" />
+      <div className="github-logo">
+        <a href="https://github.com/Thibaut-Moncourtois">
+          <header className="absolute top-10 left-20">
+            <img className="w-10  " src={GithubLogo} alt="LogoGithub" />
           </header>
         </a>
       </div>
 
       <div className="linkedin-logo">
         <a href="https://www.linkedin.com/in/thibaut-moncourtois/">
-          <header className="absolute top-9 right left-8  ">
+          <header className="absolute top-10 right left-8  ">
             <img className="w-10" src={LinkedInLogo} alt="LogoLinkedIn" />
           </header>
         </a>
