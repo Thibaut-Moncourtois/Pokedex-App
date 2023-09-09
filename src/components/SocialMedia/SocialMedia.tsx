9530978d20c2b1b1a4b058993a260520f349ba22
Thibaut <thibaut.moncourtois@gmail.com>
2023-09-09 10:07:59 +0200

    mise en place du logo Linkedin

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
 import Xlogo from '@assets/x-logo.png';
+import LinkedInLogo from '@assets/linkedin-logo.png';
 
 function socialMedia() {
   return (
-    <div>
-      <header className="absolute top-10 left-20">
-        <img className="w-10" src={Xlogo} alt="logo" />
-      </header>
-    </div>
+    <section className="logos">
+      <div className="x-logo">
+        <header className="absolute top-9 left-20">
+          <img className="w-9" src={Xlogo} alt="logoX" />
+        </header>
+      </div>
+
+      <div className="linkedin-logo">
+        <header className="absolute top-9 left-7">
+          <img className="w-10" src={LinkedInLogo} alt="LogoLinkedIn" />
+        </header>
+      </div>
+    </section>
   );
 }
 
